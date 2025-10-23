c712136509d33ab83b0e9ff3bdccfd0351c6b205
ocnaibill <biancocosta01@gmail.com>
2025-10-23 11:25:19 -0300

    fix: changes for app compilation

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // A raiz do nosso código front-end está na pasta 'client'
   root: 'client',
+  base: './', // <-- Mantenha isso, é crucial
   plugins: [react()],
-  // Configurações do servidor de desenvolvimento
   server: {
-    // A porta em que o Vite vai rodar
     port: 5173,
-    // Impede o Vite de limpar o console, para vermos os logs do Electron
     clearScreen: false,
   },
-  // Configurações para quando formos "buildar" o projeto para produção
   build: {
-    // Onde os arquivos finais serão salvos
-    outDir: '../../dist',
+    // --- CORREÇÃO AQUI ---
+    // Como o 'root' é 'client', o 'outDir: "dist"' salvará
+    // os arquivos na pasta 'concord/client/dist/'.
+    outDir: 'dist', 
   },
 });
